21bff81f19468221087733f409810c298c6e25df
Niyat Debesay <niyatdebesay26@gmail.com>
2024-07-18 23:39:27 +0300

    Create palindrome numbers

---
+++ Palindrome_Number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+       
+
+        x = str(x)
+        n = x[::-1] 
+        if x ==n:
+            return True
+        else:
+            return False
